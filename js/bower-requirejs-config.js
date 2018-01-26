5ff2d8db3a5c80abac60bf1cc0b17a2b034538eb
Lean Rada <godffrey0@gmail.com>
2018-01-26 18:58:38 +0800

    Deploy

---
+++ js/bower-requirejs-config.js
@@ -0,0 +1,15 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    jquery: "../jquery/dist/jquery",
+    "jquery-bez": "../jquery-bez/jquery.bez.min",
+    jquery_appear: "../jquery_appear/jquery.appear",
+    requirejs: "../requirejs/require",
+    underscore: "../underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
